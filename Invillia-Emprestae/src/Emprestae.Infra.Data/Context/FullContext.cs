e356ce2283db6865e731424a9867264f43db8aef
Geraldo Neto <netogan@gmail.com>
2020-12-29 17:55:51 -0300

    implementação do EF Core

---
+++ Invillia-Emprestae/src/Emprestae.Infra.Data/Context/FullContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Emprestae.Infra.Data.Context
+{
+    public class FullContext : DbContext
+    {
+        public IDbContextTransaction Transaction { get; set; }
+
+        public FullContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- Invillia-Emprestae/src/Emprestae.Services.API/Startup.cs
+++ Invillia-Emprestae/src/Emprestae.Services.API/Startup.cs
@@ -1,7 +1,9 @@
+using Emprestae.Infra.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,9 @@ namespace Emprestae.Services.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<FullContext>(opt => opt.UseSqlServer(connectionString));
+
             services.AddControllers();
 
             //TODO Injeção de dependência
